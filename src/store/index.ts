7276ac648c9ae391313af601a9f79a824dea33eb
zhouxin <xin.zhou@duobei.com>
2021-01-07 16:02:44 +0800

    fix: 修改推流分辨率height为480

--- src/store/index.ts
+++ src/store/index.ts
@@ -25,7 +25,7 @@ const store = Store.create({
   settingsStore: {
     defaultVideoPlayerConfig: { mirror: false, fit: VideoFitMode.Contain },
     videoInitConfig1: {
-      encoderConfig: { width: 640, height: 360, frameRate: 15 },
+      encoderConfig: { width: 640, height: 480, frameRate: 15 },
       optimizationMode: VideoOptimizationMode.Motion,
     },
     micId: DeviceID.empty,
